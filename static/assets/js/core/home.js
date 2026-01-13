f037bd7bb45cb827ec7ea9b9f979821a867eb28f
mullincap <j@mullincap.com>
2026-01-12 22:30:21 -0700

    updated home chart

--- static/assets/js/core/home.js
+++ static/assets/js/core/home.js
@@ -222,7 +222,7 @@ window.loadMainChart = function(days) {
                             },
 
                             yaxis: {
-                                min: 17000,
+                                min: 15000,
                                 max: max => max * 1.3,
                                 labels: {
                                     formatter: v => "$" + Math.round(v).toLocaleString(),
